784a38f051d249862d045743bee9c45d57738b23
Tamar Nachmany <tamar@tumblr.com>
2015-12-10 14:14:32 -0500

    Set up sample project using SwiftLint

--- Debugging-SwiftLint-Configuration-YML/ViewController.swift
+++ Debugging-SwiftLint-Configuration-YML/ViewController.swift
@@ -12,14 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        breakLinterRules()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func breakLinterRules() {
+        
+        // Linter should not give a trailing whitespace violation above ^
+        
+        // Linter should catch the colon spacing below (colon spacing rule) as well as CGRectMake (legacy constructor rule)
+        let testView:UIView = UIView.init(frame: CGRectMake(0, 0, 50, 50))
+        self.view.addSubview(testView)
+
+        // Linter should not catch the TODO below (as it's disabled in the config file)
+        
+        // TODO:
+        // Something something
     }
-
-
 }
 
