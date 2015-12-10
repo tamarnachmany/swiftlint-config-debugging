e707225236e7de05be1d6850e4c9546633cfecd0
Tamar Nachmany <tamar@tumblr.com>
2015-12-10 13:58:26 -0500

    Initial Commit

---
+++ Debugging-SwiftLint-Configuration-YML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Debugging-SwiftLint-Configuration-YML
+//
+//  Created by Tamar Nachmany on 12/10/15.
+//  Copyright © 2015 Tamar Nachmany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
